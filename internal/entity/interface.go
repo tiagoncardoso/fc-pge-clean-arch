b9db549f28039c076c27ffb1080774b03f94cf1a
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2024-11-09 11:42:07 -0300

    chore: remove unused interface

--- internal/entity/interface.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type OrderRepositoryInterface interface {
-	Save(order *Order) error
-	// GetTotal() (int, error)
-	FindOrderById(id string) (*Order, error)
-	FindOrders() ([]*Order, error)
-}
